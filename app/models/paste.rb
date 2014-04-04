8652c543aade40c51111b2ac1f7e1a0ce29aac72
Julien Legros <julien.jay@gmail.com>
2014-04-04 14:43:32 +0200

    body limit 250 kchars

--- app/models/paste.rb
+++ app/models/paste.rb
@@ -1,7 +1,7 @@
 class Paste < ActiveRecord::Base
   attr_accessor :truncated
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 250000 }
 
   paginates_per 10
 end
