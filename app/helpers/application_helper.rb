0f7c2a1ce356320e649c379fc4a74becf5ad8d2a
Julien Legros <julien.jay@gmail.com>
2014-04-08 16:58:11 +0200

    title, description tags; layout fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
   module BootstrapExtension
     FORM_CONTROL_CLASS = "form-control"
 
--- app/models/paste.rb
+++ app/models/paste.rb
@@ -2,6 +2,7 @@ class Paste < ActiveRecord::Base
   attr_accessor :truncated
 
   validates :body, presence: true, length: { maximum: 250000 }
+  validate :title, length: { maximum: 500 }
 
   paginates_per 10
 end
